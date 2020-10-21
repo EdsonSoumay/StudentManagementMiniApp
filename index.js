3195cf547ad43b663772585ba46f5915b023bf89
dennis28billy <70142682+dennis28billy@users.noreply.github.com>
2020-10-21 14:18:53 +0800

    update filter function

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const prodi = document.querySelector('#prodi');
 const tableSiswa = document.querySelector('.table');
 const id = document.getElementById('id');
 const name = document.getElementById('name');
+var body = tableSiswa.querySelector('tbody');
+var filterfak = document.querySelector('#filterFakultas');
+var filterpro = document.querySelector('#filterProdi');
 const formulir = document.querySelector('.formulir');
 let tipe = [document.querySelector('#male'),
             document.querySelector('#female')]
@@ -29,14 +32,18 @@ function tambah(events){
       datas.td2.innerText = name.value;
       datas.td3.innerText = gender;
       datas.td4.innerText = fakultas.value;
-      datas.tbtn.innerText = prodi.value;
+      datas.td5.innerText = prodi.value;
+      datas.tbtn.classname = 'btn btn-danger';
+      datas.tbtn.textContent = 'delete';
 
       datas.rows.appendChild(datas.td1);
       datas.rows.appendChild(datas.td2);
       datas.rows.appendChild(datas.td3);
+      datas.rows.appendChild(datas.td4);
       datas.rows.appendChild(datas.td5);
       datas.rows.appendChild(datas.tbtn);
-      tableSiswa.appendChild(datas.rows);
+
+      body.appendChild(datas.rows);
 }
 
   formulir.style.display = 'none';
@@ -46,5 +53,41 @@ function tambah(events){
       formulir.style.display = 'block'
     } else{
       formulir.style.display = "none"
+    }
+  }
+  function carinama(){
+    var nilainama = cari.value.toUpperCase();
+    var pencarian = body.getElementsByTagName('tr');
+    for (var i = 0; i < pencarian.length; i++) {
+      var dataDalamRow = pencarian[i].getElementsByTagName('td')[1];
+      if (dataDalamRow.textContent.toUpperCase().includes(nilainama)>0) {
+        pencarian[i].style.display = ''
+      }else{
+        pencarian[i].style.display = 'none'
+      }
+    }
+  }
+  function filterfakultas(){
+    var nilainama = filterfak.value.toUpperCase();
+    var pencarian = body.getElementsByTagName('tr');
+    for (var i = 0; i < pencarian.length; i++) {
+      var dataDalamRow = pencarian[i].getElementsByTagName('td')[3];
+      if (dataDalamRow.textContent.toUpperCase().includes(nilainama)>0) {
+        pencarian[i].style.display = ''
+      }else{
+        pencarian[i].style.display = 'none'
+      }
+    }
+  }
+  function filterprodi(){
+    var nilainama = filterpro.value.toUpperCase();
+    var pencarian = body.getElementsByTagName('tr');
+    for (var i = 0; i < pencarian.length; i++) {
+      var dataDalamRow = pencarian[i].getElementsByTagName('td')[4];
+      if (dataDalamRow.textContent.toUpperCase().includes(nilainama)>0) {
+        pencarian[i].style.display = ''
+      }else{
+        pencarian[i].style.display = 'none'
+      }
     }
   }
